83f254bd647b49386fb4b9d081c5b1020a622a53
Kunal Upadya <ku16@duke.edu>
2019-06-05 17:38:17 -0400

    finished the api

---
+++ APIserver/serializers.py
@@ -0,0 +1,7 @@
+# from rest_framework import serializers
+# from .models import AudioSample
+#
+# class FileSerializer(serializers.ModelSerializer):
+#     class Meta():
+#         model = AudioSample
+#         fields = ('procedure, location, test_audio','user_id', 'timestamp','api_key','test_video')
