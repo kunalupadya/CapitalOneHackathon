d1bee4e5a016b00d0baa3b18edb995181eb09718
Kunal Upadya <ku16@duke.edu>
2019-06-06 03:43:57 -0400

    please work

--- APIserver/views.py
+++ APIserver/views.py
@@ -12,31 +12,31 @@ class FileView(APIView):
     parser_classes = (MultiPartParser, FormParser)
 
     def post(self, request, *args, **kwargs):
-        try:
-            procedure = request.data.get('procedure')
-            print("1")
-            location = request.data.get('location')
-            print("2")
-            price = request.data.get('price')
-            print("3")
-            age = request.data.get('age')
-            print("4")
-            days = request.data.get('daysFromToday')
-            print("5")
-            paths = ['pce_results_2018_en.xlsx', 'uk data.xlsx', "cleanhospitals/AtlantiCare.xlsx", 'cleanhospitals/AuroraHealth.xlsx','cleanhospitals/DukeHospital.xlsx', 'cleanhospitals/MarthasVinyard.xlsx', 'cleanhospitals/MountSinai.xlsx', 'cleanhospitals/OrlandoHealth.xlsx']
-            print("6")
-            costs = ['america_list.xlsx', 'canada_list.xlsx']
-            print("7")
-            my_kb = kb.KB(paths, costs, 15)
-            print("8")
-            results = my_kb.search(procedure, age, days, price)
-            print("9")
+        # try:
+        procedure = request.data.get('procedure')
+        print("1")
+        location = request.data.get('location')
+        print("2")
+        price = request.data.get('price')
+        print("3")
+        age = request.data.get('age')
+        print("4")
+        days = request.data.get('daysFromToday')
+        print("5")
+        paths = ['pce_results_2018_en.xlsx', 'uk data.xlsx', "cleanhospitals/AtlantiCare.xlsx", 'cleanhospitals/AuroraHealth.xlsx','cleanhospitals/DukeHospital.xlsx', 'cleanhospitals/MarthasVinyard.xlsx', 'cleanhospitals/MountSinai.xlsx', 'cleanhospitals/OrlandoHealth.xlsx']
+        print("6")
+        costs = ['america_list.xlsx', 'canada_list.xlsx']
+        print("7")
+        my_kb = kb.KB(paths, costs, 15)
+        print("8")
+        results = my_kb.search(procedure, age, days, price)
+        print("9")
             # ret = ""
             # ret += procedure+"\n"
             # ret += location + "\n"
             # ret += price + "\n"
             # ret += age + "\n"
             # ret += date + "\n"
-        except:
-            return Response("Invalid request", status=status.HTTP_400_BAD_REQUEST)
+        # except:
+        #     return Response("Invalid request", status=status.HTTP_400_BAD_REQUEST)
         return Response(results, status=status.HTTP_200_OK)
