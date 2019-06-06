5857fd8aadca412475a712e0250bd59819855774
Kunal Upadya <ku16@duke.edu>
2019-06-06 10:47:53 -0400

    please work

--- APIserver/views.py
+++ APIserver/views.py
@@ -19,28 +19,49 @@ class FileView(APIView):
         # print(request.data)
         # print(data)
         procedure = data['procedure']
-        print(procedure)
-        print("1")
+        # print(procedure)
+        # print("1")
         location = data['location']
-        print(location)
-        print("2")
+        # print(location)
+        # print("2")
         price = data['price']
-        print(price)
-        print("3")
+        # print(price)
+        # print("3")
         age = data['age']
-        print(age)
-        print("4")
+        # print(age)
+        # print("4")
         days = data['surgeryDate']
-        print(days)
-        print("5")
+        # print(days)
+        # print("5")
         paths = ['pce_results_2018_en.xlsx', 'uk data.xlsx', "cleanhospitals/AtlantiCare.xlsx", 'cleanhospitals/AuroraHealth.xlsx','cleanhospitals/DukeHospital.xlsx', 'cleanhospitals/MarthasVinyard.xlsx', 'cleanhospitals/MountSinai.xlsx', 'cleanhospitals/OrlandoHealth.xlsx']
-        print("6")
+        # print("6")
         costs = ['america_list.xlsx', 'canada_list.xlsx']
-        print("7")
+        # print("7")
         my_kb = kb.KB(paths, costs, 15)
-        print("8")
+        # print("8")
         results = my_kb.search(procedure, age, days, price)
-        print("9")
+        results = sorted(results, key=lambda k: k['Cost'])
+        for each in results:
+            if each['Location'] == '1 Hospital Rd, Oak Bluffs, MA 02557':
+                each['coords'] = {'lat':41.460258, 'lng':-70.583038}
+            elif each['Location'] == '1414 Kuhl Ave, Orlando, FL 32806':
+                each['coords'] = {'lat':28.525920, 'lng':-81.377910}
+            elif each['Location'] == '1468 Madison Ave, New York, NY 10029':
+                each['coords'] = {'lat':40.790310, 'lng':-73.952103}
+            elif each['Location'] == '2301 Erwin Rd, Durham, NC 27710':
+                each['coords'] = {'lat':36.007359, 'lng':-78.937439}
+            elif each['Location'] == '2845 Greenbrier Rd, Green Bay, WI 54311':
+                each['coords'] = {'lat':44.475250, 'lng':-87.940560}
+            elif each['Location'] == 'Atlantic City, New Jersey, United States':
+                each['coords'] = {'lat':39.360610, 'lng': -74.431880}
+            elif each['Location'] in ["Greater London", "Buckinghamshire", "Cambridgeshire", "Durham", "East Riding of Yorkshire", "East Sussex ",
+                    "Essex", "Gloucestershire", "Greater Manchester","Halton", "Hampshire", "Hartlepool",
+                 "Herefordshire", "Oxfordshire", "Rutland", "Shropshire", "Slough", "Somerset"]:
+                each['coords'] = {'lat': 51.507351, 'lng': -0.127758}
+            else:
+                each['coords'] = {'lat': 56.130367, 'lng': -106.346771}
+
+        # print("9")
         print(results)
             # ret = ""
             # ret += procedure+"\n"
